1044acb31181174aafd6369289f11619c689c85f
Daniel Raban <danieltraban@gmail.com>
2024-05-05 17:16:17 +0100

    feat: add about me copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import React from "react";
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">about me</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        🚀 Senior Software Engineer by profession, explorer by passion. I thrive
+        in the ever-evolving world of technology, specializing in full stack
+        development with a toolkit encompassing Java, Angular, React,
+        TypeScript, Python, and NodeJS. With a penchant for problem-solving and
+        a knack for crafting elegant solutions, I'm constantly pushing
+        boundaries to create innovative software solutions that leave a lasting
+        impact.
+        <br />
+        <br />
+        🎶 Beyond the lines of code, I'm deeply entrenched in the pulsating
+        rhythms of techno and house music, where every beat tells a story.
+        Cycling through the city streets, attending electrifying concerts, and
+        jet-setting to new destinations fuel my adventurous spirit. Having
+        called both the US and the UK home, I've savored the diverse flavors of
+        Atlanta's BBQ and tacos, while also relishing the beauty of British
+        landscapes.
+        <br />
+        <br />
+        💡 Embracing life with unyielding honesty and tenacity, I see each
+        setback as a stepping stone for growth. I approach every challenge with
+        an open mind, viewing failure not as defeat, but as an opportunity for
+        learning and refinement. With a philosophy rooted in seizing every
+        moment and immersing myself in different cultures, I'm on a perpetual
+        journey of self-discovery and exploration.
       </p>
     </section>
   );
