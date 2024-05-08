b3539ef81749d654127a26a91189a4e774f36992
Daniel Raban <danieltraban@gmail.com>
2024-05-08 15:39:13 +0100

    feat: add badges to about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import Badge from '../components/badge';
 import Link from 'next/link';
 import React from 'react';
 
@@ -8,38 +9,97 @@ export default function Page() {
       <p className="prose prose-neutral dark:prose-invert">
         🚀 Senior Software Engineer by profession, explorer by passion. I thrive
         in the ever-evolving world of technology, specializing in full stack
-        development with a toolkit encompassing Java, Angular, React,
-        TypeScript, Python, and NodeJS. With a penchant for problem-solving and
-        a knack for crafting elegant solutions, I'm constantly pushing
-        boundaries to create innovative software solutions that leave a lasting
-        impact.
-        </p>
-        <Link
-          key={'/work'}
-          href={'/work'}
-          className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
-        >
-          view my cv
-        </Link>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <p className="prose prose-neutral dark:prose-invert">
+        development with a toolkit encompassing{' '}
+        <span className="not-prose">
+          <Badge href="https://spring.io/">
+            <img
+              className="!mr-1"
+              style={{ width: 14, height: 14 }}
+              src="/spring-boot.svg"
+            />
+            Java
+          </Badge>
+        </span>
+        ,{' '}
+        <Badge href="https://angular.io">
+          <img
+            className="!mr-1"
+            style={{ width: 14, height: 14 }}
+            src="/angular.svg"
+          />
+          Angular
+        </Badge>
+        ,{' '}
+        <Badge href="https://react.dev">
+          <svg
+            width="14"
+            height="14"
+            role="img"
+            aria-label="React logo"
+            className="!mr-1"
+          >
+            <use href="/sprite.svg#react" />
+          </svg>
+          React
+        </Badge>
+        ,{' '}
+        <Badge href="https://typescriptlang.org">
+          <img
+            className="!mr-1"
+            style={{ width: 14, height: 14 }}
+            src="/ts.svg"
+          />
+          Typescript
+        </Badge>
+        ,{' '}
+        <Badge href="https://typescriptlang.org">
+          <img
+            className="!mr-1"
+            style={{ width: 14, height: 14 }}
+            src="/python.svg"
+          />
+          Python
+        </Badge>
+        , &{' '}
+        <Badge href="https://nodejs.org/">
+          <img
+            className="!mr-1"
+            style={{ width: 14, height: 14 }}
+            src="/nodejs.svg"
+          />
+          Node
+        </Badge>
+        . With a penchant for problem-solving and a knack for crafting elegant
+        solutions, I'm constantly pushing boundaries to create innovative
+        software solutions that leave a lasting impact.
+      </p>
+      <Link
+        key={'/work'}
+        href={'/work'}
+        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
+      >
+        view my cv
+      </Link>
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      <p className="prose prose-neutral dark:prose-invert">
         🤖 Delving into the realm of artificial intelligence has become a
         newfound passion of mine. Beyond just coding, I'm fascinated by the
         ethical implications of AI and its potential to reshape our world.
         Exploring the ethical dimensions of AI is not just a professional
         interest but a personal mission, as I believe in leveraging technology
         responsibly for the betterment of society.
-        </p>
-        <Link
-          key={'https://github.com/danielraban/'}
-          href={'https://github.com/danielraban/'}
-          rel="noopener noreferrer" target="_blank"
-          className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
-        >
-          check out my github
-        </Link>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <p className="prose prose-neutral dark:prose-invert">
+      </p>
+      <Link
+        key={'https://github.com/danielraban/'}
+        href={'https://github.com/danielraban/'}
+        rel="noopener noreferrer"
+        target="_blank"
+        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
+      >
+        check out my github
+      </Link>
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      <p className="prose prose-neutral dark:prose-invert">
         🎶 Beyond the lines of code, I'm deeply entrenched in the pulsating
         rhythms of techno and house music, where every beat tells a story.
         Cycling through the city streets, attending electrifying concerts, and
@@ -47,16 +107,16 @@ export default function Page() {
         called both the US and the UK home, I've savored the diverse flavors of
         Atlanta's BBQ and tacos, while also relishing the beauty of British
         landscapes.
-        </p>
-        <Link
-          key={'/music'}
-          href={'/music'}
-          className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
-        >
-          listen to my sounds
-        </Link>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <p className="prose prose-neutral dark:prose-invert">
+      </p>
+      <Link
+        key={'/music'}
+        href={'/music'}
+        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
+      >
+        listen to my sounds
+      </Link>
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      <p className="prose prose-neutral dark:prose-invert">
         🎨 AI art is another frontier I've eagerly embraced, marveling at the
         fusion of creativity and algorithms. Through Python-powered LLMs (Large
         Language Models), I'm delving into the limitless possibilities of
@@ -64,21 +124,21 @@ export default function Page() {
         of expression and creativity. Blending technology with artistry, I'm
         captivated by the collaborative dance between human ingenuity and
         machine intelligence.
-        </p>
-        <Link
-          key={'/artwork'}
-          href={'/artwork'}
-          className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
-        >
-          see my artwork
-        </Link>
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        ⚽ Living in the heart of East London, I'm not only surrounded by the
-        vibrant pulse of the city but also by the spirit of West Ham United
-        Football Club. Supporting my local team is a passion that runs deep,
-        adding an extra layer of excitement to my life.
-        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
-        <p className="prose prose-neutral dark:prose-invert">
+      </p>
+      <Link
+        key={'/artwork'}
+        href={'/artwork'}
+        className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1"
+      >
+        see my artwork
+      </Link>
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      ⚽ Living in the heart of East London, I'm not only surrounded by the
+      vibrant pulse of the city but also by the spirit of West Ham United
+      Football Club. Supporting my local team is a passion that runs deep,
+      adding an extra layer of excitement to my life.
+      <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+      <p className="prose prose-neutral dark:prose-invert">
         💡 Embracing life with unyielding honesty and tenacity, I see each
         setback as a stepping stone for growth. I approach every challenge with
         an open mind, viewing failure not as defeat, but as an opportunity for
