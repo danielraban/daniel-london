6afa3404856d127254ec6c820d079afc35e8d1f9
Daniel Raban <danieltraban@gmail.com>
2024-05-22 18:35:47 +0100

    feat: spotify integration WIP

---
+++ app/api/spotify/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { getRecentlyPlayed } from '../../lib/spotify';
+
+export async function GET(req: NextRequest) {
+  console.log(req)
+  try {
+    const data = await getRecentlyPlayed();
+    console.log('data', data)
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Unable to fetch recently played tracks' }, { status: 500 });
+  }
+}
